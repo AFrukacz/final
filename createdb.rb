da50afad7eda644af590c632ebc1a4ab03f31af5
AFrukacz <afrukacz@gmail.com>
2020-03-13 23:57:53 +0000

    1

--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,38 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :places do
   primary_key :id
   String :title
   String :description, text: true
   String :date
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :comments do
   primary_key :id
-  foreign_key :event_id
+  foreign_key :places_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+places_table = DB.from(:places)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+places_table.insert(title: "The BK Lounge", 
+                    description: "The ONLY place for late night eats.",
+                    location: "1740 Orrington Rd")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+places_table.insert(title: "Bat 17", 
+                    description: "Sandwiches, beers, and BIGGER sandwiches",
+                    location: "1709 Benson Ave")
+
+places_table.insert(title: "Buffalo Joe's", 
+                    description: "North Side's best wings! Period.",
+                    location: "812 Clark St")                    
